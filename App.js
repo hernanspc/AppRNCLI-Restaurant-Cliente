f76cae9121ee9f4f4641345fac17ae09fcd6157b
hernan <gemelos0406>
2022-06-12 05:35:45 -0500

    add navigator

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import NuevaOrden from './views/NuevaOrden'
+import DetallePlatillo from './views/DetallePlatillo'
+import FormularioPlatillo from './views/FormularioPlatillo'
+import ProgresoPedido from './views/ProgresoPedido'
+import ResumenPedido from './views/ResumenPedido'
 import Menu from './views/Menu'
 
 const Stack = createStackNavigator();
@@ -21,6 +26,7 @@ const App = () => {
               title:"Nueva Orden"
             }}
           />
+          
         </Stack.Navigator>
       </NavigationContainer>
     </>
