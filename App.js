07c4acbb233702261c3cb57da8830158a04deead
hernan <gemelos0406>
2022-06-12 05:34:43 -0500

    add configuration and stack

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import NuevaOrden from './views/NuevaOrden'
+import Menu from './views/Menu'
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola mundo cruel</Text>
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="Nueva Orden"
+            component={NuevaOrden}
+            options={{
+              title:"Nueva Orden"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   )
 }
 
