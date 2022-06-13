181a70195d5794e8d3028900980860d1484c842a
hernan <gemelos0406>
2022-06-12 19:26:11 -0500

    global styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import 'react-native-gesture-handler';
+import { StyleSheet, View } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -15,6 +15,9 @@ import Menu from './views/Menu'
 import FirebaseState from './context/firebase/firebaseState'
 import PedidoState from './context/pedidos/pedidoState'
 
+import { NativeBaseProvider,  } from 'native-base';
+
+
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -22,68 +25,71 @@ const App = () => {
     <>
       <FirebaseState>
         <PedidoState>
-          <NavigationContainer>
-            <Stack.Navigator
-              screenOptions={{
-                headerStyle:{
-                  backgroundColor: "#FFDA00"
-                },
-                headerTitleStyle:{
-                  fontWeight: 'bold'
-                }
-              }}
-            >
-              <Stack.Screen 
-                name="Nueva Orden"
-                component={NuevaOrden}
-                options={{
-                  title:"Nueva Orden"
-                }}
-              />
+          <NativeBaseProvider>
+            <NavigationContainer>
+              <Stack.Navigator
+                  screenOptions={{
+                    headerStyle:{
+                      backgroundColor: "#FFDA00"
+                    },
+                    headerTitleStyle:{
+                      fontWeight: 'bold'
+                    },
+                    headerTintColor: '#000'
+                  }}
+                >
+                  <Stack.Screen 
+                    name="Nueva Orden"
+                    component={NuevaOrden}
+                    options={{
+                      title:"Nueva Orden"
+                    }}
+                  />
 
-              <Stack.Screen 
-                name="Menu"
-                component={Menu}
-                options={{
-                  title:"Nuestro MenÚ"
-                }}
-              />
+                  <Stack.Screen 
+                    name="Menu"
+                    component={Menu}
+                    options={{
+                      title:"Nuestro Menú"
+                    }}
+                  />
 
-              <Stack.Screen 
-                name="DetallePlatillo"
-                component={DetallePlatillo}
-                options={{
-                  title:"Detalle Platillo"
-                }}
-              />
+                  <Stack.Screen 
+                    name="DetallePlatillo"
+                    component={DetallePlatillo}
+                    options={{
+                      title:"Detalle Platillo"
+                    }}
+                  />
 
-              <Stack.Screen 
-                name="FormularioPlatillo"
-                component={FormularioPlatillo}
-                options={{
-                  title:"Formulario Platillo"
-                }}
-              />
+                  <Stack.Screen 
+                    name="FormularioPlatillo"
+                    component={FormularioPlatillo}
+                    options={{
+                      title:"Formulario Platillo"
+                    }}
+                  />
 
-              <Stack.Screen 
-                name="ResumenPedido"
-                component={ResumenPedido}
-                options={{
-                  title:"Resumen Pedido"
-                }}
-              />
+                  <Stack.Screen 
+                    name="ResumenPedido"
+                    component={ResumenPedido}
+                    options={{
+                      title:"Resumen Pedido"
+                    }}
+                  />
 
-              <Stack.Screen 
-                name="ProgresoPedido"
-                component={ProgresoPedido}
-                options={{
-                  title:"Progreso Pedido"
-                }}
-              />
-              
+                  <Stack.Screen 
+                    name="ProgresoPedido"
+                    component={ProgresoPedido}
+                    options={{
+                      title:"Progreso Pedido"
+                    }}
+                  />
+                  
 
-            </Stack.Navigator>
-          </NavigationContainer>
+              </Stack.Navigator>
+            </NavigationContainer>
+          </NativeBaseProvider>
         </PedidoState>
       </FirebaseState>
     </>
--- views/NuevaOrden.js
+++ views/NuevaOrden.js
@@ -1,16 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Container, Button , Text,VStack} from 'native-base';
+import globalStyles from '../styles/global'
+import { useNavigation } from '@react-navigation/native';
 
 const NuevaOrden = () => {
-  console.log('ssss')
+
+  const navigation = useNavigation();
+
   return (
-    
-    <View>
-      <Text>NuevaOrden</Text>
+    <View style={globalStyles.contenedor}> 
+      <View style={[globalStyles.contenido, styles.contenido]}>
+        <Button 
+          style={globalStyles.boton}
+          rounded={15}
+          // block
+          onPress={
+            ()=>{navigation.navigate('Menu')}}
+          >
+            <Text style={globalStyles.botonTexto}>Crear nueva orden</Text>
+        </Button>
+      </View>
     </View>
   )
 }
 
 export default NuevaOrden
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contenido:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+})
