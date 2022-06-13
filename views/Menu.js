9a5ee310e4197cb890b1f359afd3571d3980447d
hernan <gemelos0406>
2022-06-12 21:43:11 -0500

    add mmenu

--- views/Menu.js
+++ views/Menu.js
@@ -5,10 +5,11 @@ import FirebaseContext from '../context/firebase/firebaseContext'
 const Menu = () => {
 
   // Context de Firebase
-  const {obtenerProductos} = useContext(FirebaseContext)
+  const {menu , obtenerProductos} = useContext(FirebaseContext)
 
   useEffect(() => {
        obtenerProductos();
+       console.log('menu ',menu)
   }, []);
 
   return (
