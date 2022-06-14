bdc14c0681d82e903da39822ccb1cc2fa26f5370
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-14 00:08:42 -0500

    add footer

--- styles/global.js
+++ styles/global.js
@@ -2,7 +2,7 @@ import { StyleSheet} from 'react-native';
 
 const globalStyles = StyleSheet.create({
     contenedor:{
-        flex:1,   
+        flex: 1,
     },
     contenido:{
         marginHorizontal: '2.5%',
@@ -15,7 +15,24 @@ const globalStyles = StyleSheet.create({
         textTransform: 'uppercase',
         fontWeight: 'bold',
         color: '#000'
+    },
+    titulo: {
+        textAlign: 'center',
+        marginTop: 40,
+        marginBottom: 20,
+        fontSize: 30,
+    },
+    image: {
+        height: 300,
+        width: '100%'
+    },
+    cantidad: {
+        paddingVertical: 20,
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
     }
+
 })
 
 export default globalStyles
--- views/DetallePlatillo.js
+++ views/DetallePlatillo.js
@@ -1,11 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import {
+  Container,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Body,
+  Text,
+  H1,
+  Box,
+  Heading,
+  Card,
+  CardItem,
+  Center
+} from 'native-base'
+import { formatUSD } from '../utils/functions'
+
+import PedidoContext from '../context/pedidos/pedidoContext'
+import globalStyles from '../styles/global'
 
 const DetallePlatillo = () => {
+
+  //pedido context
+  const { platillo } = useContext(PedidoContext);
+  const { nombre, imagen, descripcion, precio } = platillo;
+
   return (
-    <View>
-      <Text>DetallePlatillo</Text>
-    </View>
+    <Card style={[globalStyles.contenedor, { backgroundColor: '#FFF' }]}>
+      <Box style={globalStyles.contenido}>
+        <Heading style={globalStyles.titulo}>{nombre}</Heading>
+
+        <Card>
+          <Box
+            borderWidth="2"
+            background={"#FFF"}
+            borderColor="coolGray.200"
+            _dark={{
+              borderColor: "gray.600"
+            }}
+            pl="4"
+            pr="5"
+            py="4"
+          >
+            <Image style={globalStyles.image} resizeMode="contain" source={{
+              uri: imagen
+            }} alt="Alternate Text" size="md" />
+
+            <Text style={{ marginTop: 20 }}>{descripcion}</Text>
+            <Text style={globalStyles.cantidad}>Precio: S/. {formatUSD(precio)}</Text>
+
+          </Box>
+        </Card>
+      </Box>
+    </Card>
   )
 }
 
