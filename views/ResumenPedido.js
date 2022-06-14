df74ca9c2a587259d8cb3fe865c0cd1dd08ba349
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-14 17:26:52 -0500

    ResumenPedido

--- views/ResumenPedido.js
+++ views/ResumenPedido.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { StyleSheet, View, Alert } from 'react-native'
+import { StyleSheet, View, Alert, Image } from 'react-native'
 import {
   Container,
   Content,
@@ -15,6 +15,7 @@ import {
   CardItem,
   Center,
   HStack,
+  Stack,
   Form,
   Icon,
   Input,
@@ -38,7 +39,6 @@ const ResumenPedido = () => {
 
   // context de pedido
   const { pedido } = useContext(PedidoContext);
-  console.log('pedido ', pedido);
 
   return (
     <Card>
@@ -50,12 +50,37 @@ const ResumenPedido = () => {
           const { cantidad, nombre, imagen, id, precio, total } = platillo;
           return (
             <View key={id}>
-              <Text> cantidad {nombre} </Text>
-              <Text> cantidad {total} </Text>
-            </View>
+              <Stack space={3} >
+                <HStack space={3} alignItems="center">
+                  <Image style={{
+                    height: 100,
+                    width: 100
+                  }} resizeMode="contain" source={{
+                    uri: imagen
+                  }} alt="Alternate Text" size="xs" />
 
+                  <VStack
+                    space={1}
+                    divider={<Divider />}
+                    w="90%"
+                  >
+                    <HStack>
+                      <Text>{nombre}</Text>
+                    </HStack>
+                    <HStack>
+                      <Text>Cantidad: {cantidad}</Text>
+                    </HStack>
+                    <HStack>
+                      <Text>Precio: {precio}</Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+              </Stack>
+            </View>
           );
         })}
+
+        <Text style={globalStyles.cantidad}>Total a Pagar: S/. </Text>
       </>
     </Card>
   )
