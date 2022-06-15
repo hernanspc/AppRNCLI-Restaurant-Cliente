ea08b2fe995fc06e850a2a1922e230838f36ad31
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-15 00:00:19 -0500

    afinando estilos

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button } from 'native-base'
+import { Button, Card } from 'native-base'
 import globalStyles from '../styles/global'
 import { useNavigation } from '@react-navigation/native'
 import PedidoContext from '../context/pedidos/pedidoContext'
@@ -25,10 +25,27 @@ const ProgresoPedido = () => {
   }, []);
 
   return (
-    <View>
-      <Text>{idpedido}</Text>
-      <Text>{tiempo}</Text>
-    </View>
+    <Card style={[globalStyles.contenedor, { backgroundColor: '#FFF' }]}>
+      <View style={[globalStyles.contenido, { marginTop: 50 }]}>
+        {
+          tiempo === 0 && (
+            <>
+              <Text style={{ textAlign: "center" }}>Espera un momento, hemos recibido tu orden...</Text>
+              <Text style={{ textAlign: "center" }}>Estamos calculando el tiempo de entrega...</Text>
+            </>
+          )
+        }
+
+        {tiempo > 0 && (
+          <>
+            <Text style={{ textAlign: "center" }}>Su orden estará lista en: {tiempo} minutos. </Text>
+          </>
+        )}
+
+        {/* <Text>{idpedido}</Text> */}
+
+      </View>
+    </Card>
   )
 }
 
