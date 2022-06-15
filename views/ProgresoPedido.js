683f91514049f9c7588ce5157e7f0feb74a69e96
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-14 23:53:05 -0500

    progreso pedido

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -4,14 +4,30 @@ import { Button } from 'native-base'
 import globalStyles from '../styles/global'
 import { useNavigation } from '@react-navigation/native'
 import PedidoContext from '../context/pedidos/pedidoContext'
+import firebase from '../firebase'
 
 const ProgresoPedido = () => {
 
   const { idpedido } = useContext(PedidoContext);
 
+  const [tiempo, guardarTiempo] = useState(0)
+
+  useEffect(() => {
+    const obtenerProducto = () => {
+      firebase.db.collection('ordenes')
+        .doc(idpedido)
+        .onSnapshot(function (doc) {
+          guardarTiempo(doc.data().tiempoentrega)
+        })
+
+    }
+    obtenerProducto();
+  }, []);
+
   return (
     <View>
       <Text>{idpedido}</Text>
+      <Text>{tiempo}</Text>
     </View>
   )
 }
