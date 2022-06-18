e7869440a2b66cb1b434a21aa5a82378e823dbdd
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-17 23:44:07 -0500

    progreso pedido

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, Text, View, Heading } from 'react-native'
-import { Button, Card } from 'native-base'
+import { StyleSheet, View, Heading, Platform } from 'react-native'
+import { Button, Card, Text } from 'native-base'
 import globalStyles from '../styles/global'
 import { useNavigation } from '@react-navigation/native'
 import PedidoContext from '../context/pedidos/pedidoContext'
@@ -9,9 +9,8 @@ import Countdown from 'react-countdown'
 
 const ProgresoPedido = () => {
 
-  const { idpedido } = useContext(PedidoContext);
-
   const navigation = useNavigation();
+  const { idpedido } = useContext(PedidoContext);
 
   const [tiempo, guardarTiempo] = useState(0)
   const [completado, guardarCompletado] = useState(false)
@@ -24,7 +23,6 @@ const ProgresoPedido = () => {
           guardarTiempo(doc.data().tiempoentrega);
           guardarCompletado(doc.data().completado);
         })
-
     }
     obtenerProducto();
   }, []);
@@ -41,49 +39,51 @@ const ProgresoPedido = () => {
   return (
     <Card style={[globalStyles.contenedor, { backgroundColor: '#FFF' }]}>
       <View style={[globalStyles.contenido, { marginTop: 50 }]}>
+
         {
-          tiempo === 0 && (
+          tiempo === 0 ? (
             <>
               <Text style={{ textAlign: "center" }}>Espera un momento, hemos recibido tu orden...</Text>
               <Text style={{ textAlign: "center" }}>Estamos calculando el tiempo de entrega...</Text>
             </>
-          )
+          ) : null
         }
 
-        {!completado && tiempo > 0 && (
+        {!completado && tiempo > 0 ? (
           <>
-            <Text style={{ textAlign: "center" }}>Su orden estará lista en: </Text>
-            <Text>
+            <Text style={[{ textAlign: "center" }, Platform.OS === "ios" ? '' : { paddingTop: 20 }]}>Su orden estará lista en: </Text>
+            <Text style={[{ textAlign: "center" }, Platform.OS === "ios" ? '' : { paddingTop: 20 }]}>
               <Countdown
                 date={Date.now() + tiempo * 60000}
                 renderer={renderer}
               />
             </Text>
           </>
-        )}
+        ) : null}
 
-
-        {completado && (
+        {completado ?
           <>
             <Text style={[styles.textoCompletado, { textAlign: "center", fontWeight: "bold", fontSize: 25 }]}>Orden Lista</Text>
             <Text style={[styles.textoCompletado, { textAlign: "center", fontSize: 15 }]}>Por favor pase a recojer su pedido</Text>
 
-            <Button style={[globalStyles.boton, { marginBottom: 100 }]}
-              rounded
-              _dark={2}
-              block
+            <Button
+              size="sm"
+              variant={"solid"}
+              backgroundColor={"#facc15"}
+              _text={{
+                color: "#000",
+                fontWeight: 'bold',
+              }}
               onPress={() => {
                 navigation.navigate('NuevaOrden')
               }}
+              px="3"
             >
-              <Text style={[globalStyles.botonTexto]}>Comenzar una Orden Nueva</Text>
+              Nueva Orden
             </Button>
-
-          </>
-        )}
-
-        {/* <Text>{idpedido}</Text> */}
-
+          </> :
+          <></>
+        }
       </View>
     </Card>
   )
